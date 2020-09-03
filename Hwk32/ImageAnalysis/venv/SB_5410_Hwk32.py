cf69604721c236230dfccb90e29c9a8a8ffbafe2
scottbing <sbing52@comcast.net>
2020-09-02 20:05:59 -0600

    SB_5410_HW3.2
    SB_5410_HW3.2

--- Hwk32/ImageAnalysis/venv/SB_5410_Hwk32.py
+++ Hwk32/ImageAnalysis/venv/SB_5410_Hwk32.py
@@ -4,6 +4,8 @@ from SortFunctions import quickSort
 from SortFunctions import mergeSort
 from SearchFunctions import binarySearchSub
 
+import colorsys
+
 
 def comparePixels(pix1, pix2):
     return pix1[0][0] > pix2[0][0]
@@ -18,21 +20,38 @@ def storePixels(im):
 
     # store ppixels inout double tuple format.
     pixel_array = []
+    yiq_pixels = []
 
     for i in range(width):  # for loop for x position
         for j in range(height):  # for loop for y position
             # get r and g and b values of pixel at position
             r, g, b = im.getpixel((i, j))
+            #yiq conversion is expecting float between 0-1
+            yiq = colorsys.rgb_to_yiq(r/255,g/255,b/255)
+            yiq_pixels.append([yiq, (i,j)])
             pixel_array.append([(r, g, b), (i, j)])
         # end for j
     # end for i
-    return pixel_array
+    return (pixel_array, yiq_pixels) #return both lists in a tuple
 # end def storePixels(im):
 
 def pixelsToImage(im, pixels):
     outimg = Image.new("RGB", im.size)
-    outimg.putdata([p[0] for p in pixels])
-    outimg.show()
+
+    #list element 0, tuple value 0, inner tuple value 0
+    if type(pixels[0][0][0]) == float:  #dealing with YIQ
+        print("yiq")
+        yiq_out = []
+        for p in pixels:
+            r,g,b = colorsys.yiq_to_rgb(p[0][0], p[0][1], p[0][2])
+            #rgb comes out of conversion as 0-1 float
+            r,g,b = int(r*255), int(g*255), int(b*255)
+            yiq_out.append((r,g,b))
+        outimg.putdata(yiq_out)
+        #end for p in pixels:
+    else:   #dealing with RGB
+        outimg.putdata([p[0] for p in pixels])
+    #outimg.show()
     return outimg
 # end def pixelsToImage(im, pixels):
 
@@ -58,20 +77,25 @@ def main():
     # open image
     # read each pixel into memory as the image object im
     with Image.open(IMG_NAME + '.jpg') as im:
-        pixels = storePixels(im)    #store rgb pixels
-        sorted_pixels = pixels.copy()   #copy pixels fro sorting
-        selectionSort(sorted_pixels, comparePixels) #sort on first val
+        pixels,yiq_pixels = storePixels(im)    #store rgb pixels
+
+        selectionSort(yiq_pixels, comparePixels) #sort on first val
         ## may need sorted image to see what is going on
-        #sorted_im = pixelsToImage(im, sorted_pixels)
-        #sorted_im.save('sorted_' + IMG_NAME + '.jpg', 'JPEG')
+        sorted_im = pixelsToImage(im, yiq_pixels)
+        sorted_im.save('sorted_' + IMG_NAME + '.jpg', 'JPEG')
 
         grayScale(im, pixels)   #grayscale pixels in place
-        threshold = 128 #define threshold to throw away pixels after
-        subi = binarySearchSub([r[0][0] for r in sorted_pixels],
-                               0, len(sorted_pixels)-1, threshold)
+        #replace threshold with target color to pivot around
+        target = (183/255, 198/255, 144/255)    # /255 for conversion
+        yiq_target = colorsys.rgb_to_yiq(target[0], target[1], target[2])
+
+        #use yiq_target instead  of threshold in search
+        subi = binarySearchSub([r[0][0] for r in yiq_pixels],
+                               0, len(yiq_pixels)-1, yiq_target[0])
+        print(subi)
         # subi = binarySearchSub([r[0][0] for r in sorted_pixels],
         #                        0, len(sorted_pixels) - 1, threshold)
-        pixelsToPoints(im, sorted_pixels[0:subi])  #put saved pixels on gray
+        pixelsToPoints(im, yiq_pixels[0:subi])  #put saved pixels on gray
         #pixelsToPoints(im, sorted_pixels[subi:])
         im.show()
     # end with Image.open(IMG_NAME + '.jpg') as im:
