ebffa8fc2656c9c90fe28ea92420463b7d9b48d6
scottbing <sbing52@comcast.net>
2020-09-03 15:43:50 -0600

    SB_5410_HW32
    SB_5410_HW32

--- Hwk32/ImageAnalysis/venv/SB_5410_Hwk32.py
+++ Hwk32/ImageAnalysis/venv/SB_5410_Hwk32.py
@@ -3,111 +3,126 @@ from SortFunctions import selectionSort
 from SortFunctions import quickSort
 from SortFunctions import mergeSort
 from SearchFunctions import binarySearchSub
-
+from PixelFunctions import *
 import colorsys
+import os
 
+TOGGLE_SLICE = False
+TOLERENCE = False
+RED = 183
+GREEN = 198
+BLUE = 144
 
-def comparePixels(pix1, pix2):
-    return pix1[0][0] > pix2[0][0]
+#Check for numeric and 0-255
+#https://stackoverflow.com/questions/31684083/validate-if-input-string-is-a-number-between-0-255-using-regex
+def is_number(n):
+    try:
+        int(n)
+        return True
+    except ValueError:
+        return False
 
-
-# end def comparePixels(pix1,pix2):
-
-
-def storePixels(im):
-    width = int(im.size[0])
-    height = int(im.size[1])
-
-    # store ppixels inout double tuple format.
-    pixel_array = []
-    yiq_pixels = []
-
-    for i in range(width):  # for loop for x position
-        for j in range(height):  # for loop for y position
-            # get r and g and b values of pixel at position
-            r, g, b = im.getpixel((i, j))
-            #yiq conversion is expecting float between 0-1
-            yiq = colorsys.rgb_to_yiq(r/255,g/255,b/255)
-            yiq_pixels.append([yiq, (i,j)])
-            pixel_array.append([(r, g, b), (i, j)])
-        # end for j
-    # end for i
-    return (pixel_array, yiq_pixels) #return both lists in a tuple
-# end def storePixels(im):
-
-def pixelsToImage(im, pixels):
-    outimg = Image.new("RGB", im.size)
-
-    #list element 0, tuple value 0, inner tuple value 0
-    if type(pixels[0][0][0]) == float:  #dealing with YIQ
-        print("yiq")
-        yiq_out = []
-        for p in pixels:
-            r,g,b = colorsys.yiq_to_rgb(p[0][0], p[0][1], p[0][2])
-            #rgb comes out of conversion as 0-1 float
-            r,g,b = int(r*255), int(g*255), int(b*255)
-            yiq_out.append((r,g,b))
-        outimg.putdata(yiq_out)
-        #end for p in pixels:
-    else:   #dealing with RGB
-        outimg.putdata([p[0] for p in pixels])
-    #outimg.show()
-    return outimg
-# end def pixelsToImage(im, pixels):
-
-def pixelsToPoints(im, pixels):
-    #default background color is black
-    for p in pixels:
-        if type(p[0][0]) == float:  #YIQ value
-            im.putpixel(p[1],
-                        tuple([int(v*255) for v in colorsys.yiq_to_rgb(p[0][0], p[0][1], p[0][2])]))
-        else:
-            im.putpixel(p[1], p[0])
-    #im.show()
-# enddef pixelsToPoints(im, pixels):
-
-def grayScale(im, pixels):
-    draw = ImageDraw.Draw(im)
-    for px in pixels:
-        gray_av = int((px[0][0] + px[0][1] + px[0][2])/3)
-        draw.point(px[1], (gray_av, gray_av, gray_av))
-#end of def grayScale(im, pixels):
 
 def main():
-    IMG_NAME = 'tinyrose'
+    global TOGGLE_SLICE
+    global RED
+    global GREEN
+    global BLUE
+    global TOLERENCE
 
-    # open image
-    # read each pixel into memory as the image object im
-    with Image.open(IMG_NAME + '.jpg') as im:
-        pixels,yiq_pixels = storePixels(im)    #store rgb pixels
+    while (True):
 
-        #selectionSort(yiq_pixels, comparePixels) #sort on first val
-        mergeSort(yiq_pixels, comparePixels) #sort on first val
-        ## may need sorted image to see what is going on
-        sorted_im = pixelsToImage(im, yiq_pixels)
-        sorted_im.save('sorted_' + IMG_NAME + '.jpg', 'JPEG')
+        IMG_NAME = 'tinyrose'   #tiny rose
+        #IMG_NAME = 'bigrose'   #big rose
 
-        grayScale(im, pixels)   #grayscale pixels in place
-        #replace threshold with target color to pivot around
-        target = (183/255, 198/255, 144/255)    # /255 for conversion
-        yiq_target = colorsys.rgb_to_yiq(target[0], target[1], target[2])
+        # open image
+        # read each pixel into memory as the image object im
+        with Image.open(IMG_NAME + '.jpg') as im:
+            pixels, yiq_pixels = storePixels(im)  # store rgb pixels
 
-        #use yiq_target instead  of threshold in search
-        subi = binarySearchSub([r[0][0] for r in yiq_pixels],
-                               0, len(yiq_pixels)-1, yiq_target[0])
-        print(subi)
-        # subi = binarySearchSub([r[0][0] for r in sorted_pixels],
-        #                        0, len(sorted_pixels) - 1, threshold)
-        pixelsToPoints(im, yiq_pixels[0:subi])  #put saved pixels on gray
-        #pixelsToPoints(im, sorted_pixels[subi:])
-        im.show()
-    # end with Image.open(IMG_NAME + '.jpg') as im:
+            # selectionSort(yiq_pixels, comparePixels) #sort on first val
+            mergeSort(yiq_pixels, comparePixels)  # sort on first val
+            ## may need sorted image to see what is going on
+            sorted_im = pixelsToImage(im, yiq_pixels)
+            sorted_im.save('sorted_' + IMG_NAME + '.jpg', 'JPEG')
 
-    # save my image data from memory to a file with a different name
-    im.save('highlighted_' + IMG_NAME + '.jpg', 'JPEG')
+            grayScale(im, pixels)  # grayscale pixels in place
+            # replace threshold with target color to pivot around
+            target = (int(RED) / 255, int(GREEN) / 255, int(BLUE) / 255)  # /255 for conversiono
+            print("RED: ", RED)
+            print("GREEN: ", GREEN)
+            print("BLUE: ", BLUE)
+            yiq_target = colorsys.rgb_to_yiq(target[0], target[1], target[2])
+
+            if TOLERENCE:
+                tolerance = int(len(yiq_pixels)/2)
+                print("Tolerance: ", tolerance)
+                # change the pivot point
+                subi = tolerance
+                TOLERENCE = False   #reset tolerance
+            else:
+                # use yiq_target instead  of threshold in search
+                subi = binarySearchSub([r[0][0] for r in yiq_pixels],
+                                        0, len(yiq_pixels) - 1, yiq_target[0])
+
+            print("subi:", subi)
+            # subi = binarySearchSub([r[0][0] for r in sorted_pixels],
+            #                        0, len(sorted_pixels) - 1, threshold)
+            if (TOGGLE_SLICE):
+                pixelsToPoints(im, yiq_pixels[0:subi])  # put saved pixels on gray
+            else:
+                pixelsToPoints(im, yiq_pixels[subi:])
+            im.show()
+        # end with Image.open(IMG_NAME + '.jpg') as im:
+
+        # command = input("Type (Q|q) to save file and quit:")
+        # if (command in ('q', 'Q')):
+        #     # save my image data from memory to a file with a different name
+        #     im.save('highlighted_' + IMG_NAME + '.jpg', 'JPEG')
+        #     break
+
+        #taken from: https://stackoverflow.com/questions/34192588/simple-menu-in-python-3
+        choice = '0'
+        while choice == '0':
+            print("Main Choice: Choose 1 of 5 choices")
+            print("Choose '(Q|q)' to save file and quit:")
+            print("Choose '(R|r)' to reverse the image")
+            print("Choose '(T|t)' to set image Tolerance")
+            print("Choose '(C|c)' to set image's RGB value")
+
+            choice = input("Please make a choice: ")
+            if choice in ('q', 'Q'):
+                # save my image data from memory to a file with a different name
+                im.save('highlighted_' + IMG_NAME + '.jpg', 'JPEG')
+                quit()
+            elif choice in ('r', 'R'):
+                TOGGLE_SLICE = not TOGGLE_SLICE     # toggle
+                print("Do 'R'")
+            elif choice in ('t', 'T'):
+                TOLERENCE = True
+            elif choice in ('c', 'C'):
+                r = input("Enter a value for red: ")
+                if is_number(r):
+                    RED = r
+                else:
+                    print("Invalid value for Red")
+                g = input("Enter a value for green: ")
+                if is_number(g):
+                    GREEN = g
+                else:
+                    print("Invalid value for Green")
+                b = input("Enter a value for blue: ")
+                if is_number(b):
+                    BLUE = b
+                else:
+                    print("Invalid value for Blue")
+            else:
+                print("I don't understand your choice.")
 
     # opens with your external preiview program, shows memory representaion
-    #im.show()
+    # im.show()
+
+
 # end of def main():
 
 if __name__ == "__main__":
